2266d974299f6370cb1cbf183cbaebcb3b3c551f
Anantha Natarajan <sananthanatarajan12@gmail.com>
2015-04-05 16:07:48 +0530

    :wrench: Fix travis

--- test/test.py
+++ test/test.py
@@ -1,6 +1,19 @@
+from sys import version_info
+from time import sleep
+import argparse
 from sauron.main import *
 from nose.tools import eq_, ok_
-
+if version_info.major is 2:
+    import pynotify
+    notifyModule = "pynotify"
+    if version_info.major is 3:
+        try:
+            from gi.Repository import Notify
+            notifyModule = "Notify"
+        except ImportError:
+            import notify2
+            notifyModule = "notify2"
+    
 def test_intern():
     eq_(pop("x","y",""), True)
 
